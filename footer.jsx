1f49d2924603e788180414908a268e3bc8e4d6a8
Tsaihemanth150 <90019941+Tsaihemanth150@users.noreply.github.com>
2021-11-03 16:38:44 +0530

    Create footer.jsx
    this about footer in website

---
+++ footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer=()=>{
+    return(
+        <>
+        <footer className= "bg-light text-center">
+            <p>
+                Health and life insurance:HLI-307                 
+               <br /> This webiste is made according to project given by K L university.
+            </p>
+
+        </footer>
+        </>
+    )
+}
+export default Footer;
