7d7b9fe676ad6cf5c0e2a228700998d998ecc298
Tsaihemanth150 <90019941+Tsaihemanth150@users.noreply.github.com>
2021-11-03 16:26:34 +0530

    Create home.js
    This file for home page

---
+++ home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Common from './Common';
+import { NavLink } from 'react-router-dom';
+
+const Home =() => {
+   return (
+     <>
+  <center><p><Common name="A Health and life insurance :"/></p></center> 
+     </>
+   )
+}
+
+export default Home;
